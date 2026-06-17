5a0bee12d10d8e7f8b554d8f649c179409bce468
dibyojyoti05 <dibyojyoti562005bal@gmail.com>
2026-06-17 10:24:44 +0000

    operation done

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -17,3 +17,30 @@ let someNumber=33
 let ConvertTostring=String(someNumber)
 console.log(typeof someNumber)
 console.log(typeof ConvertTostring)
+
+// ******************* Operations ****************
+
+let value = 3
+let negvalue=-value
+// console.log(negvalue)
+
+let str1="hello"
+let str2=" world"
+let str3=str1+str2
+// console.log(str1+str2)
+// console.log("1"+2)
+// console.log("1"+"2")
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log(3+4-5*6/1)
+ let num1,num2,num3
+ num1=num2=num3=2+2
+//  console.log(num1)
+//  console.log(num2)
+//  console.log(num3)
+
+let gamecounter=100
+++gamecounter
+console.log(gamecounter)
