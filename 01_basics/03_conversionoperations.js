6a53c474f6881c0068d2929e70686abcca39ec51
dibyojyoti05 <dibyojyoti562005bal@gmail.com>
2026-06-17 08:14:26 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,19 @@
+// let score = true
+// console.log(typeof score);
+// valueinNumber=Number(score)
+// console.log(typeof valueinNumber);
+// console.log(valueinNumber)
+
+// "33"=> 33
+// "33abc"=> NAN (not a number) but value is number.
+// true => 1 , false => 0
+// ""=> false
+// "hello"=> true
+// let isLoggedIn=""
+// let ConvertToBoolean=Boolean(isLoggedIn)
+// console.log(ConvertToBoolean)
+
+let someNumber=33
+let ConvertTostring=String(someNumber)
+console.log(typeof someNumber)
+console.log(typeof ConvertTostring)
